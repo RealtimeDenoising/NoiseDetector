a82f954d801bae73e0f177bb2b3131e37682c2a5
altanai <tara181989@gmail.com>
2022-12-01 20:52:04 -0800

    update notebooks for work, credits, contents and licenses

--- NoiseTest.py
+++ NoiseTest.py
@@ -15,34 +15,23 @@ def is_valid(image):
     # Calculate percentage of pixels with saturation >= p
     p = 0.05
     s_perc = np.sum(s[int(p * 255):-1]) / np.prod(image.shape[0:2])
-
-    ##### Just for visualization and debug; remove in final
+ 
     plt.plot(s)
     plt.plot([p * 255, p * 255], [0, np.max(s)], 'r')
     plt.text(p * 255 + 5, 0.9 * np.max(s), str(s_perc))
     plt.show()
-    ##### Just for visualization and debug; remove in final
 
     # Percentage threshold; above: valid image, below: noise
-    s_thr = 0.5
+    s_thr = 0.2  # 20% of pixels with saturation >= p
     return s_perc > s_thr
 
 
 # Read example images; convert to grayscale
 # noise1 = cv2.cvtColor(io.imread('https://i.stack.imgur.com/Xz9l0.png'), cv2.COLOR_RGB2BGR)
-# noise2 = cv2.cvtColor(io.imread('https://i.stack.imgur.com/9ZPAj.jpg'), cv2.COLOR_RGB2BGR)
-# valid = cv2.cvtColor(io.imread('https://i.stack.imgur.com/0FNPQ.jpg'), cv2.COLOR_RGB2BGR)
 
-# for img in [noise1, noise2, valid]:
-#     print(is_valid(img))
-
-
-# Read example images; convert to grayscale
-# noise1 = cv2.cvtColor(io.imread('https://i.stack.imgur.com/Xz9l0.png'), cv2.COLOR_RGB2BGR)
-noise2 = cv2.cvtColor(cv2.imread("C:\\Users\\abisht\\Downloads\\RealtimeDenoising\\WebRTCCapturesDataset\\GT\\im2.png"), cv2.COLOR_RGB2BGR)
+noise2 = cv2.cvtColor(cv2.imread("GT.png"), cv2.COLOR_RGB2BGR)
+noise2 = cv2.cvtColor(cv2.imread("Noisy.jpeg"), cv2.COLOR_RGB2BGR)
 # noise3 = cv2.cvtColor(io.imread('C:\Users\abisht\Downloads\RealtimeDenoising\WebRTCCapturesDataset\GT\3.png'), cv2.COLOR_RGB2BGR)
 
-# for img in [noise1, noise2, noise3]:
-#     print(is_valid(img))
-
 print(is_valid(noise2))
+print(is_valid(noise3))
